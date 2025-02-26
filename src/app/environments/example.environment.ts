bbab682b3b2a8a61db4216d9ee10d2365764dab0
gokhanaker <gokhanaker@gmail.com>
2025-02-26 20:39:16 +0300

    refactor: added a sample environment.ts file

---
+++ src/app/environments/example.environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'API_KEY',
+    authDomain: 'AUTH_DOMAIN',
+    databaseURL: 'DATABASE_URL',
+    projectId: 'PROJECT_ID',
+    storageBucket: 'STORAGE_BUCKET',
+    messagingSenderId: 'MESSAGING_SENDER_ID',
+    appId: 'APP_ID',
+    measurementId: 'MEASUREMENT_ID'
+  }
+};
