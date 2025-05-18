8431eb7f2a8b79bf80eaae85c1cc1d1d4f21b643
gokhanaker <gokhanaker@gmail.com>
2025-05-18 19:39:09 +0300

    style: changed styling of this mini project to look more user friendly and more modern

--- src/app/components/order-sent/order-sent.component.ts
+++ src/app/components/order-sent/order-sent.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OrderService } from '@services/order/order.service';
 
@@ -7,7 +7,7 @@ import { OrderService } from '@services/order/order.service';
   templateUrl: './order-sent.component.html',
   styleUrls: ['./order-sent.component.css']
 })
-export class OrderSentComponent {
+export class OrderSentComponent implements OnInit {
   today: string;
   orderId: string;
   orderAddress: string;
@@ -16,10 +16,28 @@ export class OrderSentComponent {
   constructor(private orderService: OrderService, private router: Router) {}
 
   ngOnInit() {
-    this.today = new Date().toDateString();
-    this.orderId = this.orderService.getOrderData().orderId;
-    this.orderAddress = this.orderService.getOrderData().address;
-    this.totalPrice = this.orderService.getOrderData().totalPurchasePrice;
+    this.today = new Date().toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    });
+
+    const orderData = this.orderService.getOrderData();
+
+    this.orderId = orderData.orderId;
+    this.orderAddress = orderData.address;
+    this.totalPrice = orderData.totalPurchasePrice;
+  }
+
+  getEstimatedDeliveryDate(): string {
+    const deliveryDate = new Date();
+    deliveryDate.setDate(deliveryDate.getDate() + 5); // Assuming 5 days delivery time
+    return deliveryDate.toLocaleDateString('en-US', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric'
+    });
   }
 
   backToStore() {
